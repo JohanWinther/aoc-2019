6d228c2c7c37e5eadaa838ae70b78b08aaadcd67
Johan Winther <johan.winther@outlook.com>
2019-12-14 00:10:26 +0100

    Better test printing

--- D9.py
+++ D9.py
@@ -8,16 +8,17 @@ t = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
 m = [int(n) for n in t.split(',')]
 computer = Intcode(m, name="BOOST")
 computer.run()
-print("Test 1")
-print(computer.output_buffer)
-print(m)
-print(computer.output_buffer == m)
+out = computer.output_buffer
+print("Test 1:")
+print(out)
+print(out == m, end="\n\n")
 
 # Test 2
 t = "1102,34915192,34915192,7,4,7,99,0"
 m = [int(n) for i, n in enumerate(t.split(','))]
 computer = Intcode(m, name="BOOST")
 computer.run()
-print("Test 2")
-print(computer.output_buffer)
-
+print("Test 2:")
+out = computer.output_buffer[0]
+print(out)
+print(len(str(out)) == 16, end="\n\n")
