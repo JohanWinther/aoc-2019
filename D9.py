d0994f5e23b00cd61e63d071fa3fc1ddf24dfa53
Johan Winther <johan.winther@outlook.com>
2019-12-14 00:01:48 +0100

    First D9.1 test

--- D9.py
+++ D9.py
@@ -1,6 +1,12 @@
 from intcode import Intcode
 
-with open(__file__.replace(".py","I.txt")) as f:
-    m = [int(n) for n in f.readline().rstrip().split(',')]
+with open(__file__.replace(".py", "I.txt")) as f:
+    t = f.readline().rstrip()
+
+t = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+m = [int(n) for i, n in enumerate(t.split(','))]
 
 computer = Intcode(m, name="BOOST")
+computer.run()
+
+print(computer.output_buffer)
