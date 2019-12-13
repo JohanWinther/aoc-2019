ddfd084c976ef81d2ccec5db574175925b4443e1
Johan Winther <johan.winther@outlook.com>
2019-12-14 00:08:01 +0100

    Passing D9.1 test 1 & 2

--- D9.py
+++ D9.py
@@ -3,10 +3,21 @@ from intcode import Intcode
 with open(__file__.replace(".py", "I.txt")) as f:
     t = f.readline().rstrip()
 
+# Test 1
 t = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
-m = [int(n) for i, n in enumerate(t.split(','))]
-
+m = [int(n) for n in t.split(',')]
 computer = Intcode(m, name="BOOST")
 computer.run()
-
+print("Test 1")
 print(computer.output_buffer)
+print(m)
+print(computer.output_buffer == m)
+
+# Test 2
+t = "1102,34915192,34915192,7,4,7,99,0"
+m = [int(n) for i, n in enumerate(t.split(','))]
+computer = Intcode(m, name="BOOST")
+computer.run()
+print("Test 2")
+print(computer.output_buffer)
+
